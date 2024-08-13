540413ef193d5fa3958544b427e938a6a108c9dd
gamonges <gamonges.engineer@gmail.com>
2024-08-13 16:06:11 +0900

    dynamicImport

--- 16_rest_api/src/070_dynamic_import/start/Example.jsx
+++ 16_rest_api/src/070_dynamic_import/start/Example.jsx
@@ -1,5 +1,11 @@
+import { add } from "./add";
+
 const Example = () => {
-  
+  const dynamicImport = async () => {
+		const module = await import("./add");
+		console.log(module);
+	}
+	dynamicImport();
 };
 
 export default Example;
